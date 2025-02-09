4d452338f9e7da6d6196557450c9b6270be51c71
nik2168 <128375663+nik2168@users.noreply.github.com>
2025-02-09 21:36:52 +0530

    added selection sort algorithm

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ 
+int main(){
+
+    int arr[] = {4, 9, 2, 1, 3};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i = 0; i < n; i++){
+
+        int min_idx = i;  // just assume first as the smallest ele
+        for(int j = i + 1; j < n; j++){
+          if(arr[min_idx] > arr[j]) min_idx = j;  // if first smallest is greator then someone update it
+        }
+        swap(arr[i], arr[min_idx]); // make sure the smallest in the array comest int he sorted portion
+    }
+
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}

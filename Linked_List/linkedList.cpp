0545e10aa00f507051faf8eddc233a61c874008e
nik2168 <128375663+nik2168@users.noreply.github.com>
2025-03-04 15:30:42 +0530

    added linked list

--- Linked_List/linkedList.cpp
+++ Linked_List/linkedList.cpp
@@ -1,81 +1,218 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class node{
+class node
+{
 
 public:
+    int val;
+    node *next;
+    node *pre;
 
-int val;
-node * next;
-
-node(int val){
-    this->val = val;
-    next = NULL;
-}
-
+    node(int val)
+    {
+        this->val = val;
+        next = NULL;
+        pre = NULL;
+    }
 };
 
-class list{
+class list
+{
 
-    public:
-    
-    node * head;
+public:
+    node *head;
+    node *tail;
 
-    list(){
+    int size;
+
+    list()
+    {
         head = NULL;
+        tail = NULL;
+        size = 0;
     }
 
-    void insertNode(int val){
-        node * newNode = new node(val);
+    void insertAtTail(int val)
+    {
+        node *newNode = new node(val);
 
-        if(head == NULL){
+        if (head == NULL)
+        {
             head = newNode;
+            tail = newNode;
+            size++;
             return;
         }
 
-        node * t = head;
-        while(t->next != NULL){
+        node * last = tail;
+        last->next = newNode;
+        newNode->pre = last;
+        tail = newNode;
+        size++;
+    }
+
+    void insertAtHead(int val)
+    {
+
+        node *newNode = new node(val);
+
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            size++;
+            return;
+        }
+
+        node *first = newNode;
+        first->next = head;
+        head->pre = first;
+        head = first;
+        size++;
+    }
+
+    void insertAtPosition(int pos, int val)
+    {
+
+        // optimization
+        /*  head->1->2->3->4->5<-tail  */
+
+        node *newNode = new node(val);
+
+        if (head == NULL && tail == NULL)
+        {
+            if (pos == 0)
+            {
+                head = newNode;
+                tail = newNode;
+                return;
+            }
+            else
+            {
+                cout << "List is already empty ! try to insert at 0th position" << endl;
+                return;
+            }
+        }
+
+        int itr;
+
+        int mid = size / 2;
+
+        if (pos <= mid)
+        {
+
+            itr = 0;
+            node * temp = head;
+
+            while (itr <= mid)
+            {
+                if (itr == pos - 1)
+                    break;
+                temp = temp->next;
+                itr++;
+            }
+
+            node * var = temp->next;
+            temp->next = newNode;
+            newNode->pre = temp;
+
+            newNode->next = var;
+            var->pre = newNode;
+
+            return;
+        }
+
+        itr = size;
+        node *temp = tail;
+
+        while (itr >= mid)
+        {
+            if (itr == pos - 1)
+                break;
+            temp = temp->pre;
+            itr--;
+        }
+
+        node * var = temp->pre;
+        temp->pre = newNode;
+        var->next = newNode;
+        newNode->next = temp;
+        newNode->pre = var;
+    }
+
+    void printList()
+    {
+        node *t = head;
+
+        cout<<"head->";
+        while (t != NULL)
+        {
+            cout << t->val << "<->";
             t = t->next;
         }
 
-        t->next = newNode;
-
+        cout << "NULL";
+        cout<<"<-tail"<<endl;
     }
-
-    void printList(){
-        node * t = head;
-
-        while(t != NULL){
-            cout<<t->val<<"->";
-            t = t->next;
-        }
-
-        cout<<"NULL"<<endl;
-    }
-
 };
 
-int main(){
-    
-int sizeOfList;
+int main()
+{
 
-cout<<"Enter the size of List : ";
-cin>>sizeOfList;
+    list l;
 
-list l;
-for(int i = 0; i < sizeOfList; i++){
-    int val;
-    cin>>val;
-     l.insertNode(val);
-}
+    int atEnd;
+    cout << "Enter the size of elemets wants to enter at end -> ";
+    cin >> atEnd;
 
-l.printList();
+    cout << "Enter the elements : ";
+    for (int i = 0; i < atEnd; i++)
+    {
+        int val;
+        cin >> val;
+        l.insertAtTail(val);
+    }
+
+    int atSt;
+    cout << "Enter the size of elemets wants to enter at start : ";
+    cin >> atSt;
+
+    cout << "Enter the elements : ";
+    for (int i = 0; i < atSt; i++)
+    {
+        int val;
+        cin >> val;
+        l.insertAtHead(val);
+    }
+
+    l.printList();
+
+    int inbtw;
+    cout << "Enter the size of elemets wants to enter in between : ";
+    cin >> inbtw;
+
+    cout << "Enter the elements : ";
+    for (int i = 0; i < inbtw; i++)
+    {
+        int pos, val;
+        cin >> pos;
+        cin >> val;
+        l.insertAtPosition(pos, val);
+    }
+
+    l.printList();
 
     return 0;
 }
 
-
 // output
-// Enter the size of List : 3
-// 1 2 3
-// 1->2->3->NULL
+// Enter the size of elemets wants to enter at end -> 3
+// Enter the elements : 4 5 6
+// Enter the size of elemets wants to enter at start : 3
+// Enter the elements : 3 2 1
+// head->1<->2<->3<->4<->5<->6<->NULL<-tail
+// Enter the size of elemets wants to enter in between : 5
+// Enter the elements : 0 1 2 2 3 3 4 4 5 9
+// head->1<->2<->2<->3<->3<->4<->4<->9<->5<->1<->6<->NULL<-tail
+
--- expression/InfixToPostfix.cpp
+++ expression/InfixToPostfix.cpp
@@ -73,7 +73,6 @@ string infixToPostFixConverter(string &s)
             }
     
         }
-       if(st.size()) cout<<st.top()<<" "<<postfix<<endl;
     }
 
     while (!st.empty())
@@ -97,3 +96,30 @@ int main()
 
     return 0;
 }
+
+// output : 
+/*
+Infix : a+b*(c^d-e)^(f+g*h)-i
+Postfix : abcd^e-fgh*+^*+i-%  
+*/
+
+
+// Algorithm :
+/*
+Algorithm to convert infix expression to postfix expression:
+
+1. Initialize an empty stack `st` for operators and an empty string `postfix` for the result.
+
+2. Iterate over each character `ch` in the input infix expression `s`:
+   a. If `ch` is an operand (alphanumeric character), append it to `postfix`.
+   b. If `ch` is an opening parenthesis '(', push it onto the stack `st`.
+   c. If `ch` is a closing parenthesis ')', pop from `st` and append to `postfix` until an opening parenthesis '(' is encountered on the stack. Pop and discard the '(' from the stack.
+   d. If `ch` is an operator:
+      i. While the stack is not empty, and the precedence of the operator at the top of the stack is greater than or equal to the precedence of `ch`, pop from `st` and append to `postfix`.
+      ii. Push `ch` onto the stack `st`.
+
+3. After the entire infix expression has been processed, pop and append all remaining operators from the stack `st` to `postfix`.
+
+4. The `postfix` string now contains the postfix expression equivalent of the given infix expression.
+*/
+
